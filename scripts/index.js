bfe462822d557ded1446a198c3babc8b0debd143
Kirill Karpov <rin.veber@gmail.com>
2022-03-21 21:57:11 +0400

    hotfix: исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -4,44 +4,39 @@ const closeButton = popupElement.querySelector('.popup__close-button');
 const submitButton = popupElement.querySelector('.popup__submit-button');
 
 function openPopup() {
+	nameInput.value = userName.textContent;
+	descriptionInput.value = userDescription.textContent;
+
 	popupElement.classList.remove('popup_opened');
 }
 
 function closePopup() {
+	nameInput.value = null;
+	descriptionInput.value = null;
+
 	popupElement.classList.add('popup_opened');
 }
 
 editProfile.addEventListener('click', openPopup)
 closeButton.addEventListener('click', closePopup)
+submitButton.addEventListener('click', formSubmitHandler)
 
 const userName = document.querySelector('.profile__name');
 const userDescription = document.querySelector('.profile__description'); 
 
 let editElement = document.querySelector('.popup__edit-profile');
-let nameInput = document.querySelector('.popup__name');
-let descriptionInput = document.querySelector('.popup__description');
-
-nameInput.value = userName.textContent;
-descriptionInput.value = userDescription.textContent;
+let nameInput = document.querySelector('.popup__name_input');
+let descriptionInput = document.querySelector('.popup_description_input');
 
 function formSubmitHandler (evt) {
 	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
     userName.textContent = nameInput.value;
 	userDescription.textContent = descriptionInput.value;
 
 	closePopup();
+
+	
 }
 
-editElement.addEventListener('submit', formSubmitHandler);
-
-let elementLikeList = document.querySelectorAll('.element__heart');
-
-elementLikeList.forEach((elementLike, i, a) => {
-    elementLike.addEventListener('click', () => {
-        elementLike.classList.toggle('element__heart_active');
-    })
-}); 
-    
 
       
