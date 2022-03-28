1f6635d6b5b63460e8303e438b15026eb3a529cc
Kirill Karpov <rin.veber@gmail.com>
2022-03-28 17:06:22 +0400

    develop: разработка попапов карточек

--- scripts/CreateCard.js
+++ scripts/CreateCard.js
@@ -7,14 +7,14 @@ const inputPlaceLink = addPopup.querySelector('.popup__input_type_placeLink');
 const closeButtonAdd = addPopup.querySelector('.popup__close-button');
 
 const cardElements = document.querySelector('.elements__list');
-const card = document.querySelector('#card').content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 function openPopupAdd() {
 	addPopup.classList.add('popup_opened');
 }
 
 function closePopupAdd() {
-	addPopup.classList.remove('.popup_opened');
+	addPopup.classList.remove('popup_opened');
 }
 
 cardAdd.addEventListener('click', openPopupAdd)
@@ -24,37 +24,46 @@ function formSubmitHandlerAdd(evt) {
 	evt.preventDefault();
 	addCard();
 	closePopupAdd();
-};
+}
 
 formCard.addEventListener('submit', formSubmitHandlerAdd);
 
+function addCard() {
+	cardElements.prepend(createCard({
+		name: inputPlaceName.value,
+		link: inputPlaceLink.value,
+	}))
+}
 function createCard(name, link) {
-	const newCard = card.querySelector('.element').cloneNode(true);
+	const newCard = cardTemplate.querySelector('.card').content.firstElementChild.cloneNode(true);
 
 	const elementName = newCard.querySelector('.element__title');
 	elementName.textContent = name;
 
-	const elementImage = newCard.querySelector('.element__photo');
-	elementImage.alt = name;
-	elementImage.src = link;
-	elementImage.addEventListener('click', function () {
-		openShowPhotoPopup({ name, link });
-	});
+	const elementPhoto = newCard.querySelector('.element__photo');
+	elementPhoto.querySelector('.element__title').textContent = name;
+	elementPhoto.querySelector('.element__photo').src = link;
+  
+	const like = newCard.querySelector('.element__heart');
+	like.addEventListener('click',  function(evt) {
+		evt.target.classList.toggle('element__heart_active')});
+
 	return newCard;
 }
 
-function addCard() {
-	cardElements.prepend(createCard({
-		name = inputPlaceName.value,
-		link = inputPlaceLink.value,
-	}))
+function openPhoto (evt) {
+	photoFull.src = evt.target.src;
+	photoCaption.textContent = evt.target.alt;
+	openPopup(popupPhoto);
 }
 
+const renderCards = (cards) => (
+	cards.reverse().forEach((card) => cardElements.append(createCard(card)))
+  );
+  renderCards(initialCards);
 let elementLikeList = document.querySelectorAll('.element__heart');
 elementLikeList.forEach((elementLike, i, a) => {
 	elementLike.addEventListener('click', () => {
 		elementLike.classList.toggle('element__heart_active');
 	})
 });
-
-
