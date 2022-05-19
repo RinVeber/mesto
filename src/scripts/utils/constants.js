f58158da5be433e84833e14dc52b74016f1039c7
Kirill Karpov <rin.veber@gmail.com>
2022-05-19 10:30:32 +0400

    fix: удаление подключение стилей из htm

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -1,7 +1,11 @@
 const sakura = new URL('../../image/card_1.jpg', import.meta.url);
 const jinx = new URL('../../image/card_2.jpeg', import.meta.url);
-const kosmos = new URL('../../image/card_3.jpg', import.meta.url)
-
+const kosmos = new URL('../../image/card_3.jpg', import.meta.url);
+const china = new URL('../../image/card_4.jpg', import.meta.url);
+const whitefox = new URL('../../image/card_5.jpg', import.meta.url);
+const zakat = new URL('../../image/card_6.jpg', import.meta.url);
+const island = new URL('../../image/card_7.jpg', import.meta.url);
+const kamchatka = new URL('../../image/card_8.jpg', import.meta.url);
 export const initialCards = [
   {
     name: 'Девушка под Сакурой',
@@ -9,27 +13,23 @@ export const initialCards = [
   },
   {
     name: 'Остров',
-    link: 'https://images.unsplash.com/photo-1509280951623-4a17506e3eb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+    link: island
   },
   {
     name: 'Белый лис',
-    link: 'https://images.unsplash.com/photo-1484312152213-d713e8b7c053?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    link: whitefox
   },
   {
     name: 'Закат',
-    link: 'https://cdn.pixabay.com/photo/2016/05/25/03/17/sunset-1413848_1280.jpg'
+    link: zakat
   },
   {
     name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Улица в Чикаго',
-    link: 'https://images.unsplash.com/photo-1515963665762-77ef90e624fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80'
+    link: kamchatka
   },
   {
     name: 'Китай, Императоры Ян и Хуан',
-    link: 'https://images.unsplash.com/photo-1632367652130-a5ac609f4e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+    link: china
   },
   {
     name: 'Джинкс',
